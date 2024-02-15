610854aa5c707c8cdac801ffa3d3ccd701ec5dd8
SeanM96 <sean-9708@hotmail.co.uk>
2024-02-15 11:36:53 +0000

    feat(checkout): remove item, multi item type test

--- cmd/checkout/checkout/checkout.go
+++ cmd/checkout/checkout/checkout.go
@@ -1,42 +1,56 @@
 package checkout
 
+import "log"
+
 type PricingRule struct {
-  UnitPrice int
-  SpecialPrice int
-  SpecialAmount int
+	UnitPrice     int
+	SpecialPrice  int
+	SpecialAmount int
 }
 
 type Checkout struct {
-  pricingRules map[string]PricingRule
-  scannedItems map[string]int
+	pricingRules map[string]PricingRule
+	scannedItems map[string]int
 }
 
 func NewCheckout(pricingRules map[string]PricingRule) *Checkout {
-  return &Checkout{
-    pricingRules: pricingRules,
-    scannedItems: make(map[string]int),
-  }
+	return &Checkout{
+		pricingRules: pricingRules,
+		scannedItems: make(map[string]int),
+	}
 }
 
 func (c *Checkout) Scan(item string) {
-  c.scannedItems[item]++
+	if _, ok := c.pricingRules[item]; ok {
+		c.scannedItems[item]++
+	} else {
+		log.Printf("Warning:%s %s", item, "is an unrecognised item!")
+	}
+}
+
+func (c *Checkout) RemoveItem(item string) {
+	if count, ok := c.scannedItems[item]; ok && count > 0 {
+		c.scannedItems[item]--
+	} else {
+		log.Printf("Warning: %s %s", item, "cannot be removed as it has not been scanned.")
+	}
 }
 
 func (c *Checkout) GetTotalPrice() int {
-  totalPrice := 0
-  for k, v := range c.scannedItems {
-    totalPrice += calculateItemPrice(c.pricingRules[k], v)
-  }
-  return totalPrice
+	totalPrice := 0
+	for k, v := range c.scannedItems {
+		totalPrice += calculateItemPrice(c.pricingRules[k], v)
+	}
+	return totalPrice
 }
 
 func calculateItemPrice(pricingRule PricingRule, quantity int) int {
-  if pricingRule.SpecialAmount > 0 && quantity >= pricingRule.SpecialAmount {
-    numberOfSpecialPriceItemGroups := quantity / pricingRule.SpecialAmount
-    numberOfRegularPriceItems := quantity % pricingRule.SpecialAmount
+	if pricingRule.SpecialAmount > 0 && quantity >= pricingRule.SpecialAmount {
+		numberOfSpecialPriceItemGroups := quantity / pricingRule.SpecialAmount
+		numberOfRegularPriceItems := quantity % pricingRule.SpecialAmount
 
-    return (numberOfSpecialPriceItemGroups * pricingRule.SpecialPrice) + (numberOfRegularPriceItems * pricingRule.UnitPrice)
-  }
+		return (numberOfSpecialPriceItemGroups * pricingRule.SpecialPrice) + (numberOfRegularPriceItems * pricingRule.UnitPrice)
+	}
 
-  return quantity * pricingRule.UnitPrice
+	return quantity * pricingRule.UnitPrice
 }
--- cmd/checkout/checkout/checkout_test.go
+++ cmd/checkout/checkout/checkout_test.go
@@ -3,44 +3,92 @@ package checkout
 import "testing"
 
 func TestNewCheckout(t *testing.T) {
-  pricingRules := map[string]PricingRule{"A": {UnitPrice: 50, SpecialPrice: 130, SpecialAmount: 3 }}
-  checkout := NewCheckout(pricingRules)
+	pricingRules := map[string]PricingRule{"A": {UnitPrice: 50, SpecialPrice: 130, SpecialAmount: 3}}
+	checkout := NewCheckout(pricingRules)
 
-  if len(checkout.scannedItems) != 0 {
-    t.Errorf("Checkout has been created with preexisting items of length %d", len(checkout.scannedItems))
-  }
+	if len(checkout.scannedItems) != 0 {
+		t.Errorf("Checkout has been created with preexisting items of length %d", len(checkout.scannedItems))
+	}
 }
 
 func TestScanSingleItem(t *testing.T) {
-  pricingRules := map[string]PricingRule{"A": {UnitPrice: 50, SpecialPrice: 130, SpecialAmount: 3 }}
+	pricingRules := map[string]PricingRule{"A": {UnitPrice: 50, SpecialPrice: 130, SpecialAmount: 3}}
 
-  checkout := NewCheckout(pricingRules)
+	checkout := NewCheckout(pricingRules)
 
-  checkout.Scan("A")
+	checkout.Scan("A")
 
-  expectedTotal := 50
-  actualTotal := checkout.GetTotalPrice()
+	expectedTotal := 50
+	actualTotal := checkout.GetTotalPrice()
 
-  if actualTotal != expectedTotal {
-    t.Errorf("Expected total price %d, but got %d", expectedTotal, actualTotal)
-  }
+	if actualTotal != expectedTotal {
+		t.Errorf("Expected total price %d, but got %d", expectedTotal, actualTotal)
+	}
+}
+
+func TestRemoveSingleItem(t *testing.T) {
+	pricingRules := map[string]PricingRule{"A": {UnitPrice: 50, SpecialPrice: 130, SpecialAmount: 3}}
+
+	checkout := NewCheckout(pricingRules)
+
+	checkout.Scan("A")
+	checkout.Scan("A")
+	checkout.Scan("A")
+	checkout.RemoveItem("A")
+
+	expectedTotalNumItems := 2
+	expectedTotalPrice := 100
+
+	actualNumOfItems := checkout.scannedItems["A"]
+	actualTotalPrice := checkout.GetTotalPrice()
+
+	if actualNumOfItems != expectedTotalNumItems {
+		t.Errorf("Expected total number of items to be %d, but got %d", expectedTotalNumItems, actualNumOfItems)
+	}
+
+	if actualTotalPrice != expectedTotalPrice {
+		t.Errorf("Expected total price %d, but got %d", expectedTotalPrice, actualTotalPrice)
+	}
 }
 
 func TestScanMultiItem(t *testing.T) {
-  pricingRules := map[string]PricingRule{
-    "A": {UnitPrice: 50, SpecialPrice: 130, SpecialAmount: 3 },
-  }
+	pricingRules := map[string]PricingRule{
+		"A": {UnitPrice: 50, SpecialPrice: 130, SpecialAmount: 3},
+	}
 
-  checkout := NewCheckout(pricingRules)
+	checkout := NewCheckout(pricingRules)
 
-  checkout.Scan("A")
-  checkout.Scan("A")
-  checkout.Scan("A")
+	checkout.Scan("A")
+	checkout.Scan("A")
+	checkout.Scan("A")
 
-  expectedTotal := 130
-  actualTotal := checkout.GetTotalPrice()
+	expectedTotal := 130
+	actualTotal := checkout.GetTotalPrice()
 
-  if actualTotal != expectedTotal {
-    t.Errorf("Expected total price %d, but got %d", expectedTotal, actualTotal)
-  }
+	if actualTotal != expectedTotal {
+		t.Errorf("Expected total price %d, but got %d", expectedTotal, actualTotal)
+	}
+}
+
+func TestScanMultiItemTypes(t *testing.T) {
+	pricingRules := map[string]PricingRule{
+		"A": {UnitPrice: 50, SpecialPrice: 130, SpecialAmount: 3},
+		"B": {UnitPrice: 30, SpecialPrice: 45, SpecialAmount: 2},
+		"C": {UnitPrice: 20},
+	}
+
+	checkout := NewCheckout(pricingRules)
+
+	checkout.Scan("A")
+	checkout.Scan("B")
+	checkout.Scan("C")
+	checkout.Scan("A")
+	checkout.Scan("A")
+
+	expectedTotal := 180
+	actualTotal := checkout.GetTotalPrice()
+
+	if actualTotal != expectedTotal {
+		t.Errorf("Expected total price %d, but got %d", expectedTotal, actualTotal)
+	}
 }
